62071b4b00499f3ecfdb4ddc2db940bef05ebc4c
ChetanKantharia <chetankantharia@chetankantharia-System-Product-Name.(none)>
2014-11-10 13:04:31 +0530

    Initial Commint, Created HelloMessage

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+var mountNode = document.getElementById('node');
+
+var HelloMessage = React.createClass({
+	render: function(){
+		return React.createElement("div",null,"Name : ", this.props.name);
+	}
+});
+
+React.render(React.createElement(HelloMessage, {name:"Bruno"}), mountNode);
---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var port = 8080 || process.env.PORT;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port , function () {
+	console.log('On Port : ', port);
+});
